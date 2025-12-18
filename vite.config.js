fe5897ec68b9b112def863299fff7dc57dbf7f17
Huy Quoc <huy1995303@gmail.com>
2025-12-19 00:18:05 +0700

    Fix Vite preview allowedHosts for Render

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Dev local (giữ nguyên)
   server: {
     port: 3000,
     open: true
+  },
+
+  // Dùng cho Render (vite preview)
+  preview: {
+    host: true,
+    port: 10000,
+    allowedHosts: [
+      'quanlynhahang-frontend-react.onrender.com'
+    ]
   }
 })
-
