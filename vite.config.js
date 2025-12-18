a910222ae6c703f6dd1cfaaf392389cddf520f1d
Huy Quoc <huy1995303@gmail.com>
2025-12-19 00:34:35 +0700

    Fix vite preview allowedHosts for Render

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  // Dev local (giữ nguyên)
+  // Dùng khi dev local
   server: {
+    host: true,        // cho phép truy cập từ mạng
     port: 3000,
     open: true
   },
 
-  // Dùng cho Render (vite preview)
+  // Dùng khi deploy Render (vite preview)
   preview: {
-    host: true,
-    port: 10000,
+    host: true,        // bắt buộc để Render bind 0.0.0.0
+    port: 10000,       // Render mặc định
     allowedHosts: [
-      'quanlynhahang-frontend-react.onrender.com'
+      'the-crown-restaurant.onrender.com'
     ]
   }
 })
